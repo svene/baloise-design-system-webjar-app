a13183c7381e46fb6f36c419f6fe7bfd0f65eaa9
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2024-04-27 09:44:45 +0200

    setup of app with JTE controller

---
+++ src/main/java/com/example/baloisedesignsystemwebjarapp/DemoController.java
@@ -0,0 +1,19 @@
+package com.example.baloisedesignsystemwebjarapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class DemoController {
+
+	@GetMapping("/")
+	public RedirectView index() {
+		return new RedirectView("/hello");
+	}
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "app/hello";
+	}
+}
