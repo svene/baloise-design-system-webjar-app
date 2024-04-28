b1ae622d545a23b53b5786eef2b4548b2e13c5f8
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2024-04-28 08:55:56 +0200

    using a brand-icon

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,4 +1,6 @@
 import { balIconConsultant } from './webjars/baloise-design-system/16.1.0/@baloise/ds-icons/index.esm.js';
+import { balBrandIconTheftCarGreen } from './webjars/baloise-design-system/16.1.0/@baloise/ds-brand-icons/index.esm.js';
 
 document.getElementById('balIconConsultant').svg = balIconConsultant;
+document.getElementById('balBrandIconTheftCarGreen').svg = balBrandIconTheftCarGreen;
 
